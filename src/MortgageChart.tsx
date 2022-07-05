3b4101b3bc08aeed32585c0d59e4e8c7d60219fe
James Harvey <james.harvey994@gmail.com>
2022-07-05 13:20:02 +1000

    Initial commit, basic funtionality added

---
+++ src/MortgageChart.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Mortgage Simulator',
+    },
+  },
+};
+
+const makeSeries = (repayAmount: number, totalBorrowed: number, interestRate: Number): Array<Number> => {
+	let balance: Number = totalBorrowed;
+	let days = 0;
+  const returnData: Array<Number> = [];
+  returnData.push(balance);
+
+  while (balance > 0) {
+    if (days % 14 === 0) {
+      balance = +balance - repayAmount;
+    }
+    balance = Math.round((+balance + ((+balance * +interestRate / 100) / 365)) * 100) / 100;
+    days++;
+    if (days % 365 === 0) {
+      returnData.push(balance);
+    }
+    if (days >= 365 * 30) {
+      return returnData;
+    }
+  }
+  return returnData;
+};
+
+const makeData = (props: Props) => {
+  let labels: string[] = [];
+  const datasets = [];
+  console.log(props.interest);
+  for (let i = 0; i < props.scenarios; i++) {
+    const series = makeSeries(props.repayments, props.principle, props.interest + i);
+    labels = series.map((val, index) => ('Year ' + (index + 1)));
+    datasets.push({
+      label: `${props.interest + i}% Interest Rate`,
+      data: series,
+      borderColor: i === 0 ? '#3f9c35' : '#77c26e',
+      backgroundColor: i === 0 ? '#3f9c35' : '#77c26e',
+    });
+  }
+  return {
+    labels,
+    datasets,
+  };
+}
+
+interface Props {
+  principle: number;
+  repayments: number;
+  interest: number;
+  repaymentFrequency: string;
+  scenarios: number;
+}
+
+export default function MortgageChart(props: Props) {
+  return (
+    <>
+      <Line options={options} data={makeData(props)} />
+    </>
+  );
+};
---
+++ src/ResultsPage.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import MortgageChart from './MortgageChart';
+import { useSearchParams, useNavigate } from "react-router-dom";
+
+function ResultsPage() {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const principle = searchParams.get('principle');
+  const interest = searchParams.get('interest');
+  const repayments = searchParams.get('repayments');
+  const repaymentFrequency = searchParams.get('repayment_frequency') || 'fortnightly';
+  const scenarios = searchParams.get('scenarios') || 1;
+  const handleBackButton = () => {
+    navigate({
+      pathname: '/',
+      search: `?${searchParams.toString()}`,
+    });
+  };
+
+  if (
+    principle === null
+    || interest === null
+    || repayments === null
+  ) {
+    return <div className="App">Not enough data</div>
+  }
+  return (
+    <div className='bg-base-100'>
+      <div className="hero">
+        <div className="hero-content text-center">
+          <div className="max-w-lg">
+            <h1 className="text-3xl font-bold">${principle.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} at {interest}%</h1>
+            <p className="py-6">
+              Repayments of ${repayments.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} {repaymentFrequency}.
+            </p>
+            <button className='btn btn-sm' onClick={handleBackButton}>Edit</button>
+          </div>
+        </div>
+      </div>
+      <div>
+
+      
+      </div>
+      <div className='m-10'>
+        <MortgageChart
+          principle={+principle}
+          interest={+interest}
+          repayments={+repayments}
+          repaymentFrequency={repaymentFrequency}
+          scenarios={+scenarios}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ResultsPage;
